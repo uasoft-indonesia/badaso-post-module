a3776e479e6438f0fb2ea045ebde4d567da1e700
Danny Atthaya <dannyatthaya9@gmail.com>
2021-12-28 16:03:26 +0700

    Add override feature, update docs

---
+++ src/Helpers/Route.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Uasoft\Badaso\Module\Post\Helpers;
+
+class Route
+{
+    public static function getController($key)
+    {
+        // get config 'controllers' from config/badaso-post.php
+        $controllers = config('badaso-post.controllers');
+
+        // if the key is not found, return $key
+        if (!isset($controllers[$key])) {
+            return 'Uasoft\\Badaso\\Module\\Post\\Controllers\\' . $key;
+        }
+
+        // return the value of the key
+        return $controllers[$key];
+    }
+}
