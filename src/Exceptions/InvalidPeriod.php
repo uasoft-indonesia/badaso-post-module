23281406565dc6f90361476efef78d6c0c885646
miftahurrahmi <miftahurrahmi45@gmail.com>
2022-03-22 14:30:04 +0700

    phpstan badaso-post-module

--- src/Exceptions/InvalidPeriod.php
+++ src/Exceptions/InvalidPeriod.php
@@ -4,11 +4,13 @@ namespace Uasoft\Badaso\Module\Post\Exceptions;
 
 use DateTimeInterface;
 use Exception;
+use Uasoft\Badaso\Module\Post\Exceptions\InvalidPeriod as ExceptionsInvalidPeriod;
 
 class InvalidPeriod extends Exception
 {
     public static function startDateCannotBeAfterEndDate(DateTimeInterface $startDate, DateTimeInterface $endDate)
     {
-        return new static("Start date `{$startDate->format('Y-m-d')}` cannot be after end date `{$endDate->format('Y-m-d')}`.");
+        return new self("Start date `{$startDate->format('Y-m-d')}` cannot be after end date `{$endDate->format('Y-m-d')}`.");
+
     }
 }
