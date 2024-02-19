9d7d3c39fdf07af439870b7cfd12585dec0852da
miftahurrahmi <miftahurrahmi45@gmail.com>
2024-02-19 17:21:22 +0700

    update migration table comments

--- src/Migrations/2024_02_19_101638_add_new_column_to_badaso_comments_table.php
+++ src/Migrations/2024_02_19_101638_add_new_column_to_badaso_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddGuestNameToBadasoCommentsTable extends Migration
+class AddNewColumnToBadasoCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class AddGuestNameToBadasoCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::table(config('badaso.database.prefix').'comments', function (Blueprint $table) {
+        Schema::table(config('badaso.database.prefix') . 'comments', function (Blueprint $table) {
             $table->string('guest_name')->nullable()->after('parent_id');
+            $table->string('guest_email')->nullable()->after('guest_name');
+            $table->boolean('approved')->default(false)->after('content');
         });
     }
 
@@ -25,8 +27,10 @@ class AddGuestNameToBadasoCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::table(config('badaso.database.prefix').'comments', function (Blueprint $table) {
+        Schema::table(config('badaso.database.prefix') . 'comments', function (Blueprint $table) {
             $table->dropColumn('guest_name');
+            $table->dropColumn('guest_email');
+            $table->dropColumn('approved');
         });
     }
 }
